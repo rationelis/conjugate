834760751bf756cb39f525dc00a5917ddd58763b
Cornee van Blerk <corneevanblerk@gmail.com>
2021-12-03 17:03:25 +0100

    Added conjugator.com + changed structure

--- main.py
+++ main.py
@@ -6,62 +6,80 @@ import codecs
 from bs4 import BeautifulSoup
 from os.path import exists
 
-class BulkCardBuilder:
+class MasterRussianVerbs:
     def __init__(self):
-        with open("open.json") as f:
-            self.open = json.load(f)
-
-        with open("done.json") as f:
-            self.done = json.load(f)
-
-        len_open = len(self.open)
-        len_done = len(self.done)
-        print("\nStats: " + str(len_open) + " verbs to go. " + str(len_done) + " verbs done. " + str(round(len_done / len_open * 100)) + "% done!")
+        self.url = input("Paste Cooljugator of Masterrussian link: ")
 
         if not exists("cardsoftoday.txt"):
             self.file = codecs.open("cardsoftoday.txt", "w+", "utf-8")
         else:
             self.file = codecs.open("cardsoftoday.txt", "a", "utf-8")
 
-        # Pick random URL from the list.
-        custom = input("Manual link (optional, leave blank to continue): ")
-        if custom == "":
-            self.url = random.choice(range(len(self.open)))
-        else:
-            self.url = self.open.index(custom)
+        if "cooljugator.com" in self.url:
+            table = self.get_table_cooljugator(self.url)
+            self.add_cards(table)
+        elif "masterrussian.com" in self.url:
+            # with open("open.json") as f:
+            #     handle = json.load(f)
 
-        # Download URL.
-        html = self.download_url(self.open[self.url])
+            # with open("done.json") as f:
+            #     done = json.load(f)
 
-        # Go through HTML to gather conjugations.
-        table = self.get_conjugation_table(html)
+            # len_open = len(handle)
+            # len_done = len(done)
+            # print("\nStats: " + str(len_open) + " verbs to go. " + str(len_done) + " verbs done. " + str(round(len_done / len_open * 100)) + "% done!")
+           
+            table = self.get_table_masterrussian(self.url)
+            self.add_cards(table)
+        # else: # Random URL from list.
+        #     with open("open.json") as f:
+        #         handle = json.load(f)
+            
+        #     random_url = random.choice(range(len(handle)))
+        #     self.handle_masterrussian(random_url)
 
-        # self.print_table(table)
-        
-        # Add cards dialog.
-        self.add_cards(table)
-
-    def download_url(self, url):
-        r = requests.get(url)
-        # UTF-8 to support Cyrillic. 
-        r.encoding = 'utf-8'
-        return r.text
-
-    class ConjugationTable(object):
-        definition = ""
-        ipa = ""
-
-        imp_infinitive = ""
-        imp_present_conjugations = []
-        imp_past_conjugations = []
-
-        per_infinitive = ""
-        per_past_conjugations = []
-        per_future_conjugations = []
-
-    def get_conjugation_table(self, html):
+    def get_table_cooljugator(self, url):
+        html = self.download_url(url)
         soup = BeautifulSoup(html, 'html.parser')
-        
+        conjugation_table = self.ConjugationTable()
+
+        present_value = soup.find("div", {"id": "present1"})
+        if(present_value):
+            name = soup.find("span", {"id": "mainform"}).contents[0].split(" ")
+            conjugation_table.definition = name[2] + " " + name[3]
+            conjugation_table.imp_infinitive = name[0].lower()
+
+            conjugation_table.imp_present_conjugations = []
+            for i in range(0,6):
+                conjugation = soup.find("div", {"id": "present" + str(i + 1)})
+                conjugation_table.imp_present_conjugations.insert(i, conjugation["data-stressed"])
+
+            conjugation_table.imp_past_conjugations = []
+            ids = ["past_singM", "past_singF", "past_singN", "past_plur"]
+            for i in range(0,4):
+                conjugation = soup.find("div", {"id": ids[i]})
+                conjugation_table.imp_past_conjugations.insert(i, conjugation["data-stressed"])
+        else:
+            name = soup.find("span", {"id": "mainform"}).contents[0].split(" ")
+            conjugation_table.definition = name[2] + " " + name[3]
+            conjugation_table.per_infinitive = name[0].lower()
+
+            conjugation_table.per_future_conjugations = []
+            for i in range(0,6):
+                conjugation = soup.find("div", {"id": "future" + str(i + 1)})
+                conjugation_table.per_future_conjugations.insert(i, conjugation["data-stressed"])
+
+            conjugation_table.per_past_conjugations = []
+            ids = ["past_singM", "past_singF", "past_singN", "past_plur"]
+            for i in range(0,4):
+                conjugation = soup.find("div", {"id": ids[i]})
+                conjugation_table.per_past_conjugations.insert(i, conjugation["data-stressed"])
+
+        return conjugation_table
+
+    def get_table_masterrussian(self, url):
+        html = self.download_url(url)
+        soup = BeautifulSoup(html, 'html.parser')
         conjugation_table = self.ConjugationTable()  
 
         # General info
@@ -101,29 +119,49 @@ class BulkCardBuilder:
 
         return conjugation_table
 
+    def download_url(self, url):
+        r = requests.get(url)
+
+        # UTF-8 to support Cyrillic. 
+        r.encoding = 'utf-8'
+
+        return r.text
+
+    class ConjugationTable(object):
+        definition = ""
+        ipa = ""
+
+        imp_infinitive = ""
+        imp_present_conjugations = []
+        imp_past_conjugations = []
+
+        per_infinitive = ""
+        per_past_conjugations = []
+        per_future_conjugations = []
+
     def add_cards(self, table):
         print("\nRussian verb: " + table.imp_infinitive + " / " + table.per_infinitive)
-        print("Found on: " + self.open[self.url])
+        print("Found on: " + self.url)
         print("English definition: " + table.definition)
 
         # We ask for three image and sentence formats.
         delimiters_sentences = []
         images = []
 
-        print("\nProvide three card formats for this verb. Replace \P in the sentence for pronoun and \C for conjugation.")
-        images.insert(0, input("Image URL #1: "))
-        delimiters_sentences.insert(0, input("Sentence #1: "))
-        images.insert(1, input("\nImage URL #2: "))
-        delimiters_sentences.insert(1, input("Sentence #2: "))
-        images.insert(2, input("\nImage URL #3: "))
-        delimiters_sentences.insert(2, input("Sentence #3: "))
+        number_of_formats = input("\nHow many card formats: ")
 
+        print("\nProvide three card formats for this verb. Replace \P in the sentence for pronoun and \C for conjugation.")
+
+        for idx in range(0, int(number_of_formats)):
+            images.insert(idx, input("Image URL #" + str(idx + 1) + ": "))
+            delimiters_sentences.insert(idx, input("Sentence #" + str(idx + 1) + ": "))
+        
         all_cards = []
         count = [0]
 
         def get_cards_from_conjugations(conjugation_list, time):
             for idx, val in enumerate(conjugation_list):
-                # Determine whether which pronouns should be used.
+                # Determine which pronouns should be used.
                 if(len(conjugation_list) == 4): 
                     pronoun = self.get_pronoun_past(idx)
                 else:   
@@ -136,8 +174,8 @@ class BulkCardBuilder:
                     infinitive = table.imp_infinitive
 
                 # Pick index for card format
-                card_format_index = random.randint(0,2)
-                
+                card_format_index = random.randint(0,int(number_of_formats) - 1)
+
                 # Add image to card.
                 card = "<img src='" + images[card_format_index] + "?random=" + str(count[0]) + "'/>;"
 
@@ -180,16 +218,16 @@ class BulkCardBuilder:
             all_cards.remove(chosen_card)
 
         # Insert this verb to the done list.
-        self.done.insert(len(self.done), self.open[self.url])
+        # self.done.insert(len(self.done), self.open[self.url])
 
-        # # Remove this verb from the open list.
-        self.open.pop(self.url)
+        # # # Remove this verb from the open list.
+        # self.open.pop(self.url)
 
-        with open('open.json', 'w') as data_file:
-            self.open = json.dump(self.open, data_file)
+        # with open('open.json', 'w') as data_file:
+        #     self.open = json.dump(self.open, data_file)
 
-        with open('done.json', 'w') as data_file:
-            self.done = json.dump(self.done, data_file)
+        # with open('done.json', 'w') as data_file:
+        #     self.done = json.dump(self.done, data_file)
 
         print("\nDONE.")
 
@@ -229,4 +267,4 @@ class BulkCardBuilder:
         print("Perfective Future: " + ' - '.join(table.per_future_conjugations))
 
 if __name__ == '__main__':
-    BulkCardBuilder()
+    MasterRussianVerbs()
